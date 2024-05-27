348badb9926717378e48e7b890f553749c1fa2f3
SantosClaudia <146114493+SantosClaudia@users.noreply.github.com>
2024-05-27 14:03:12 +0000

    index

--- TRABALHO FINAL/js/javascript.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-function qualquerCoisa() {
-    document.getElementById("titulo").innerHTML="Adeus";
-}
-
-function trocaacor() {
-    document.getElementById("corpo").classList.add("novacor");
-}
-
